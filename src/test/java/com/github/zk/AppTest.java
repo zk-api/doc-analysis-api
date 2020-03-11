37128539ff80586c1935e5b1b54652076d54b966
e <506683059@qq.com>
2020-03-11 14:37:31 +0800

    新增FastCSV处理器

--- src/test/java/com/github/zk/AppTest.java
+++ src/test/java/com/github/zk/AppTest.java
@@ -1,8 +1,14 @@
 package com.github.zk;
 
-import static org.junit.Assert.assertTrue;
-
+import com.github.zk.csv.DealCSV;
+import com.github.zk.handler.FastCsvHandler;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -16,5 +22,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void dealCsv() throws IOException {
+        IDeal iDeal = new DealCSV();
+        Path path = Paths.get("D:\\work\\Updates_NC1.csv");
+        iDeal.createEntity(path, new String[][]{{"0","time"},{"1","proName"},{"2","cityName"},{"3","hosptail"},{"4","good"},{"5","dead"}}, new FastCsvHandler());
     }
 }
